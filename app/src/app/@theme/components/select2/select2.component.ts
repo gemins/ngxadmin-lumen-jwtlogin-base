2595707fafdbb178cb0e4fad52c3b21263c94c24
Manuel Bruña <info@gemins.com.ar>
2018-05-22 11:58:07 -0300

    app: add new class invalid for select2 control

--- app/src/app/@theme/components/select2/select2.component.ts
+++ app/src/app/@theme/components/select2/select2.component.ts
@@ -1,33 +1,38 @@
 import {Component, ElementRef, Input, Output, EventEmitter} from '@angular/core';
 import {ApiService} from '../../../@core';
 
-declare var jQuery:any;
-declare var $ :any;
+declare var jQuery: any;
+declare var $: any;
 
 import 'style-loader!./select2component.scss';
 
 
 @Component({
     selector: 'select2-component',
-    templateUrl: './select2-component.html'
+    templateUrl: './select2-component.html',
 })
 export class Select2Component {
-    @Input() id:string = 'select2';
+    @Input() id: string = 'select2';
     @Input() service;
-    @Input() defaultValue:any= {id: -1, text: "Buscar..."};
-    @Input() elementData = "data";
-    @Input() disabled = "";
+    @Input() multiple=false;
+    @Input() defaultValue: any= {id: -1, text: 'Buscar...'};
+    @Input() elementData = 'data';
+    @Input() invalid = false;
+    @Input() disabled = '';
     @Input() firstSearchAll = false;
     @Output() onChangeValue = new EventEmitter<any>();
     public emptyValue = [{id: -1, text: '', selected: 'selected', search: '', hidden: true}];
     elemSelect;
 
-    constructor(private _elementRef:ElementRef, public apiService : ApiService) {
+    constructor(private _elementRef: ElementRef, public apiService: ApiService) {
     }
 
     ngOnChanges() {
-        if (this.defaultValue && this.defaultValue.id){
-            let data = [this.defaultValue];
+        if (!this.multiple && this.defaultValue && this.defaultValue.id){
+            const data = [this.defaultValue];
+            this.createSelect2(data);
+        }else if (this.multiple && this.defaultValue){
+            const data = this.defaultValue;
             this.createSelect2(data);
         }
     }
@@ -35,27 +40,31 @@ export class Select2Component {
     ngAfterViewInit() {
         let data = [this.defaultValue];
 
-        if(this.defaultValue.id == undefined)
+        if (!this.multiple && this.defaultValue.id == undefined)
             data = this.emptyValue;
+
+        if (this.multiple)
+            data = this.defaultValue;
 
         this.createSelect2(data);
     }
 
     clearSelect(){
-        let self = this;
+        const self = this;
         this.elemSelect = this._elementRef.nativeElement.querySelector('#' + this.id);
-        jQuery(this.elemSelect).val(-1).trigger("change");
+        jQuery(this.elemSelect).val(-1).trigger('change');
     }
 
     createSelect2(data) {
-        let self = this;
+        const self = this;
         this.elemSelect = this._elementRef.nativeElement.querySelector('#' + this.id);
         jQuery(this.elemSelect).select2({
             ajax: {
-                transport:<any> function(params, success, failure){
-                    let options = {
+                transport: <any> function(params, success, failure){
+                    const options = {
                         q: params.data.term,
-                        where: self.elementData
+                        where: self.elementData,
+                        per_page: 50
                     };
                     new Promise((resolve, reject) => {
                         self.apiService.getAll(options, self.service)
@@ -65,36 +74,60 @@ export class Select2Component {
                                     resolve(result);
                                 }, result => {
                                     //failure(result);
-                                    console.log("error success");
+                                    console.log('error success');
                                 }
                             );
                     });
                 },
                 delay: 250,
-                processResults: function (data:any, params:any) {
+                processResults: function (data: any, params: any) {
                     return {
                         results: data[self.elementData].map(function (item) {
                                 return item;
                             }
-                        )
+                        ),
                     };
-                }
+                },
             },
             width: '100%',
             data: data,
             placeholder: {
                 id: -1,
-                text: "Buscar..."
+                text: 'Buscar...',
             },
-            minimumInputLength: 1
+            minimumInputLength: 1,
         }).on('select2:select', function (evt) {
             let newData = jQuery(self.elemSelect).select2('data')[0];
+
+            if(self.multiple)
+                newData = jQuery(self.elemSelect).select2('data');
+
+            self.onChangeValue.emit(newData);
+        }).on('select2:unselect', function (evt) {
+            let newData = jQuery(self.elemSelect).select2('data')[0];
+
+            if(self.multiple)
+                newData = jQuery(self.elemSelect).select2('data');
+
             self.onChangeValue.emit(newData);
         });
 
-        if (self.defaultValue.id && self.defaultValue.id != undefined)
-            jQuery(self.elemSelect).val(self.defaultValue.id).trigger("change");
-        //$(self.elemSelect).val(self.defaultValue.id, [self.defaultValue], true).trigger("change");
+
+        if (!self.multiple && self.defaultValue && self.defaultValue.id && self.defaultValue.id != undefined) {
+            jQuery(self.elemSelect).val(self.defaultValue.id).trigger('change');
+        }else if(self.multiple && self.defaultValue[0] && self.defaultValue[0].id != undefined) {
+            var selectedValues = self.parseUrls(self.defaultValue);
+            jQuery(self.elemSelect).val(selectedValues).trigger('change');
+        }
+    }
+
+    parseUrls(urls){
+        var selectedValues = [];
+        for (let url of urls){
+            url.text = url.url;
+            selectedValues.push(url.id);
+        }
+        return selectedValues;
     }
 
 }
